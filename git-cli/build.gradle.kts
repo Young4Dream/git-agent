76abda3b771c918136806183307c343550374c12
yanwei <yanwei88@aliyun.com>
2023-04-15 04:35:48 +0800

    调整打包配置

--- git-cli/build.gradle.kts
+++ git-cli/build.gradle.kts
@@ -2,22 +2,19 @@ plugins {
     java
     id("kotlin-kapt")
     kotlin("jvm") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "com.y4d"
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     implementation("com.google.protobuf:protobuf-kotlin:3.22.2")
     implementation("com.alibaba:fastjson:2.0.26")
     implementation("io.netty:netty-all:4.1.10.Final")
     implementation("info.picocli:picocli:4.7.1")
-    kapt ("info.picocli:picocli-codegen:4.7.1")
+    kapt("info.picocli:picocli-codegen:4.7.1")
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(mapOf("path" to ":")))
     testImplementation("junit:junit:4.12")
@@ -29,6 +26,7 @@ kapt {
     }
 }
 
-//compileJava {
-//    options.compilerArgs += ["-Aproject=${project.group}/${project.name}"]
-//}
+application {
+    val name = "com.y4d.git.server.GitCommandLine"
+    mainClass.set(name)
+}
--- git-server/build.gradle.kts
+++ git-server/build.gradle.kts
@@ -1,21 +1,30 @@
 plugins {
+    application
     java
+    id("kotlin-kapt")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.y4d"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    // https://mvnrepository.com/artifact/junit/junit
     testImplementation("junit:junit:4.12")
     implementation("com.google.protobuf:protobuf-kotlin:3.22.2")
     implementation("io.netty:netty-all:4.1.10.Final")
-    implementation("info.picocli:picocli-codegen:4.7.1")
+    kapt("info.picocli:picocli-codegen:4.7.1")
+    implementation("info.picocli:picocli:4.7.1")
+    implementation(project(mapOf("path" to ":")))
+}
+kapt {
+    arguments {
+        arg("project", "${project.group}/${project.name}")
+    }
+}
+
+application {
+    val name = "com.y4d.git.server.GitCommandLine"
+    mainClass.set(name)
 }
